609a5404b91f3e31f124507ac6af1def55981076
Shixiong.Liu <liushixiong@corp.netease.com>
2023-03-30 10:19:19 +0800

    修改git_about 脚本

--- git_about.sh
+++ git_about.sh
@@ -9,22 +9,22 @@ RED='\e[1;31m' # 红
 RES='\e[0m'
 
 function echo_color {
-	echo -e "${RED}$1${RES}"
+	echo -e "${RED}************$1************${RES}"
 }
 
 
-echo_color "git stash"
+echo_color "git stash start"
 git stash 
-echo_color "git stash"
+echo_color "git stash end"
 
-echo_color "git fetch up"
+echo_color "git fetch up start"
 git fetch up
-echo_color "git fetch up"
+echo_color "git fetch up end"
 
-echo_color "git rebase"
-git rebase 'up/release/v1.1.0'
-echo_color "git rebase"
+echo_color "git rebase start"
+git rebase 'youre_branch_name'
+echo_color "git rebase end"
 
-echo_color "git stash pop"
+echo_color "git stash pop start"
 git stash pop
-echo_color "git stash pop"
+echo_color "git stash pop end"
