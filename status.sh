0c3c7c195088d67515cdc3f15297ba1e3e038356
Shixiong.Liu <liushixiong@corp.netease.com>
2023-04-07 17:31:51 +0800

    增加处理.开头的文件

--- status.sh
+++ status.sh
@@ -26,14 +26,26 @@ else
 	# 遍历文件列表，设置环境变量
 	for file in "${files[@]}"
 	do
-	  # 提取出文件名（不含后缀）
-	  file_name="$(basename "$file")"
-	  file_name="${file_name%%.*}"
-	  # 设置环境变量
-	  export "$file_name=$PWD/$file"
-	  echo_red "$file_name=$PWD/$file"
+		file_name="$(basename "$file")"
+		if [[ $file_name == .* ]]; then
+			IFS='.'
+			arr=($file_name)
+			unset IFS
+			# 遍历数组中的每个元素
+			file_name="${arr[1]}"
+		else
+			# 提取出文件名（不含后缀）
+			file_name="${file_name%%.*}"
+		fi
+	# 设置环境变量
+		export "$file_name=$PWD/$file"
+		echo_red "$file_name=$PWD/$file"
 	done
 	echo_green "setting envirnoment variable end: "
 fi
 
+unset RED
+unset RES
+unset GREEN
+unset GREENEND
 
