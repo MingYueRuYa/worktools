547ab0c6bee46084896f9b98199d1f8d1611cca7
Shixiong.Liu <liushixiong@corp.netease.com>
2023-07-25 17:57:03 +0800

    重构有关shell脚本内容

--- scripts/alias_git_command.sh
+++ scripts/alias_git_command.sh
@@ -4,7 +4,7 @@ command_arr=("alias count_id='git rev-list --count '" "alias git_all_log='git lo
 			 "alias push='git push'" "alias rebase='git rebase'" "alias git_update_sub='git submodule update --init --recursive'"
 		 	 "alias fetch='git fetch'" "alias tortise_log='tortoisegitproc.exe /command:log'"
 		 	 "alias tortise_commit='tortoisegitproc.exe /command:commit'"
-		 	 "alias git_st='source git_st'")
+		 	 "alias git_st='source git_st'" "alias commit='git commit -m '")
 
 source ./echo_color_func.sh
 
