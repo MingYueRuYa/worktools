7424901dea013e7d1889fb5bf07954bc2d04991e
shixiong.Liu <studycpp1024@gmail.com>
2023-10-12 20:37:24 +0800

    增加wechat登录脚本

--- python/login_wechat.py
+++ python/login_wechat.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -7,26 +5,25 @@ import time
 import os
 import json
 
-url = 'https://www.52pojie.cn/index.php'
+
+url: str = 'https://mp.weixin.qq.com/'
 
 def start_chrome():
     chrome_path = os.getcwd() + '/chromedriver.exe'
     driver = webdriver.Chrome(service=Service(chrome_path))
     driver.implicitly_wait(10)
     driver.start_client()
+    driver.get(url)
+    driver.delete_all_cookies()
 
     path = os.getcwd() + '/cookies/'
     if not os.path.exists(path):
         os.makedirs(path)
-    file_path = path + 'cookies.txt'
+    file_path = path + 'wechat_cookies.txt'
     cookies_dict = {}
     if os.path.exists(file_path):
         file = open(file_path, 'r', encoding='utf-8')
         cookies_dict = json.load(file)
-
-    # 先加载下URL
-    driver.get(url)
-    driver.delete_all_cookies()
 
     for item in cookies_dict:
         if 'expiry' in item:
@@ -40,7 +37,6 @@ def start_chrome():
         driver.add_cookie(item)
 
     driver.get(url)
-
     return driver
 
 
@@ -48,7 +44,7 @@ def savecookies(cookies):
     path = os.getcwd() + '/cookies/'
     if not os.path.exists(path):
         os.makedirs(path)
-    with open(path + 'cookies.txt', 'w') as f:
+    with open(path + 'wechat_cookies.txt', 'w') as f:
         cookie = json.dumps(cookies)
         f.write(cookie)
 
@@ -56,13 +52,14 @@ def savecookies(cookies):
 if __name__ == "__main__":
     chrome_deriver = start_chrome()
 
-    time.sleep(10)
+    time.sleep(30)
 
-    qq_login_btn = chrome_deriver.find_element(By.XPATH, "//div[@id='um']/p[2]/a")
-    print(qq_login_btn.get_attribute('href'))
-    qq_login_btn.click()
+    # qq_login_btn = chrome_deriver.find_element(By.XPATH, "//div[@id='um']/p[2]/a")
+    # print(qq_login_btn.get_attribute('href'))
+    # qq_login_btn.click()
 
-    time.sleep(10)
+    # time.sleep(10)
     cookies = chrome_deriver.get_cookies()
     savecookies(cookies)
+
 
