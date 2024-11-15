72c2655af827dd060085ac785ec987cdacb39dca
shixiong.Liu <studycpp1024@gmail.com>
2024-11-15 15:48:47 +0800

    添加icon资源

--- env_tools/main.cpp
+++ env_tools/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[])
 {
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QApplication a(argc, argv);
+  a.setWindowIcon(QIcon(":/EnvTools/icon.png"));
   EnvTools w;
   w.show();
   return a.exec();
