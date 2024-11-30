2c63559950b61575f9957370a31792a68fc96b78
shixiong.Liu <studycpp1024@gmail.com>
2024-11-30 17:54:49 +0800

    添加中文语言

---
+++ env_tools/env_tools_zh.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>EnvToolsClass</name>
+    <message>
+        <location filename="EnvTools.ui" line="14"/>
+        <source>EnvTools</source>
+        <translation>环境变量工具</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="42"/>
+        <source>ALL:</source>
+        <translation>全部：</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="65"/>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="70"/>
+        <source>Value</source>
+        <translation>值</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="93"/>
+        <location filename="EnvTools.ui" line="162"/>
+        <source>Add</source>
+        <translation>增加</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="100"/>
+        <location filename="EnvTools.ui" line="169"/>
+        <source>Del</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="121"/>
+        <source>Key:</source>
+        <translation>Key:</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="131"/>
+        <source>Path</source>
+        <translation>Path</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="184"/>
+        <source>Tips: To apply the environment variable changes, you need to restart Explorer.</source>
+        <translation>提示：为了系统环境变量生效，需要重启explorer进程。</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="204"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="EnvTools.ui" line="211"/>
+        <source>restart explorer</source>
+        <translation>重启explorer</translation>
+    </message>
+</context>
+</TS>
